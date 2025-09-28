94dcaadaffaf5fa5e533fe4c9c5c8dfad860fed0
gabriela0402 <161508311+gabriela0402@users.noreply.github.com>
2025-09-28 15:57:08 -0300

    Add files via upload

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -21,9 +21,9 @@ const TicTacToe = () => {
 
     const checkWin = (currentBoard) => {
         const winPatterns = [
-            [0,1,2], [3,4,5], [6,7,8], // linhas
-            [0,3,6], [1,4,7], [2,5,8], // colunas
-            [0,4,8], [2,4,6]           // diagonais
+            [0,1,2], [3,4,5], [6,7,8], 
+            [0,3,6], [1,4,7], [2,5,8], 
+            [0,4,8], [2,4,6]          
         ];
 
         for (let pattern of winPatterns) {
